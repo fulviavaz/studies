66273fba0568b10d8a1d5241c8b21c691f00293a
Fulvia Vaz <fulviavaz.dev@gmail.com>
2023-07-18 13:45:29 -0300

    first commit

---
+++ src/components/Lista/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export default function Lista() {
+  const tarefas = [
+    {
+      tarefa: 'React',
+      tempo: '02:00:00'
+    },
+    {
+      tarefa: 'Angular',
+      tempo: '01:00:00'
+    },
+    {
+      tarefa: 'TypeScript',
+      tempo: '00:30:00'
+    }
+  ]
+  return (
+      <aside>
+          <h2>
+              Estudos do dia
+          </h2>
+      <ul>
+        {tarefas.map((item, index) => (
+            <li key={index}>
+                <h3>{item.tarefa}</h3>
+                <span>{item.tempo}</span>
+            </li>
+        ))}
+            
+        </ul>
+    </aside>
+  )
+}
